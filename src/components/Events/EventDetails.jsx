86b745d8fa06c6c7adef5953fd89ae87831b0014
agdc101 <agdc101@gmail.com>
2024-02-19 19:40:42 +0000

    feat: event detail data now displays

--- src/components/Events/EventDetails.jsx
+++ src/components/Events/EventDetails.jsx
@@ -1,8 +1,29 @@
-import { Link, Outlet } from 'react-router-dom';
+import { Link, Outlet, useParams } from 'react-router-dom';
+import { useQuery } from '@tanstack/react-query';
+import { fetchEvent } from '../../util/http.js';
 
 import Header from '../Header.jsx';
 
 export default function EventDetails() {
+
+  //get id from the url
+  const { id } = useParams();
+
+  const { data, isPending, isError } = useQuery({
+    queryKey: ['event', id],
+    queryFn: ({ signal }) => fetchEvent({id, signal})
+  });
+
+  if (isPending) {
+    return <p>Loading...</p>;
+  }
+
+  if (isError) {
+    return <p>Error: {isError.message}</p>;
+  }
+
+    console.log(data);
+
   return (
     <>
       <Outlet />
@@ -13,20 +34,20 @@ export default function EventDetails() {
       </Header>
       <article id="event-details">
         <header>
-          <h1>EVENT TITLE</h1>
+          <h1>{data.title}</h1>
           <nav>
             <button>Delete</button>
             <Link to="edit">Edit</Link>
           </nav>
         </header>
         <div id="event-details-content">
-          <img src="" alt="" />
+          <img src={`http://localhost:3000/${data.image}`} alt="" />
           <div id="event-details-info">
             <div>
-              <p id="event-details-location">EVENT LOCATION</p>
-              <time dateTime={`Todo-DateT$Todo-Time`}>DATE @ TIME</time>
+              <p id="event-details-location">{data.location}</p>
+              <time dateTime={`Todo-DateT$Todo-Time`}>{data.date + ' @ ' + data.time}</time>
             </div>
-            <p id="event-details-description">EVENT DESCRIPTION</p>
+            <p id="event-details-description">{data.description}</p>
           </div>
         </div>
       </article>
--- src/util/http.js
+++ src/util/http.js
@@ -62,6 +62,9 @@ export async function fetchSelectableImages({ signal }) {
 }
 
 export async function fetchEvent({ id, signal }) {
+
+  console.log(id);
+
   const response = await fetch(`http://localhost:3000/events/${id}`, { signal });
 
   if (!response.ok) {
